6bc249b46a19bc9c681192070cdeb4a890903e33
Christian Nunciato <cnunciato@chef.io>
2016-12-23 09:03:39 -0800

    Specify MIT
    Signed-off-by: Christian Nunciato <cnunciato@chef.io>

--- lib/license_scout/overrides.rb
+++ lib/license_scout/overrides.rb
@@ -521,7 +521,7 @@ module LicenseScout
         ["keymaster", "MIT", nil],
         ["loose-envify", nil, [canonical("MIT")]],
         ["natives", nil, [canonical("ISC")]],
-        ["ng2d3", nil, ["docs/README.md"]],
+        ["ng2d3", "MIT", ["docs/README.md"]],
         ["object-component", "MIT", [canonical("MIT")]],
         ["options", "MIT", ["README.md"]],
         ["over", nil, ["README.md"]],
