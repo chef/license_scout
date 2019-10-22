ce8f13cfa559571506add16f76cf97b858ea85ff
Nell Shamrell <nellshamrell@gmail.com>
2019-10-21 17:47:07 -0700

    do not fail when unable to find a transitive hab dependency
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- spec/fixtures/habitat-pkg-missing/plan.sh
+++ spec/fixtures/habitat-pkg-missing/plan.sh
@@ -4,7 +4,7 @@ pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_deps=(
+core/node
 chef/no-such-package-by-this-name
 )
 pkg_build_deps=(core/make core/gcc)
-
