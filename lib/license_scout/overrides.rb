a5aca9d29db25451c2b585409790ebcb299361b5
Your Name <you@example.com>
2022-02-09 14:01:14 +0530

    add missing license for date gem
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- lib/license_scout/overrides.rb
+++ lib/license_scout/overrides.rb
@@ -362,6 +362,7 @@ module LicenseScout
         ["coffee-script-source", nil, ["https://raw.githubusercontent.com/jessedoyle/coffee-script-source/master/LICENSE"]],
         ["compass", "MIT", ["https://raw.githubusercontent.com/Compass/compass/stable/LICENSE.markdown"]],
         ["cucumber-wire", nil, ["https://raw.githubusercontent.com/cucumber/cucumber-ruby-wire/master/LICENSE"]],
+        ["date", "BSD-2-Clause", ["https://raw.githubusercontent.com/ruby/date/master/README.md"]],
         ["dep_selector", nil, ["https://raw.githubusercontent.com/chef/dep-selector/master/LICENSE"]],
         ["enumerable-lazy", "MIT", ["https://raw.githubusercontent.com/yhara/enumerable-lazy/master/README.md"]],
         ["fast_xs", "MIT", ["https://raw.githubusercontent.com/brianmario/fast_xs/master/LICENSE"]],
