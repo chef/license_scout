6a5560e7e76468aff2880c68dec78cc00b336ce5
John McCrae <john.mccrae@progress.com>
2022-06-08 10:30:54 -0700

    updating licensing for the multipart-post gem
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -6,7 +6,7 @@ export USER="root"
 
 echo "--- bundle install"
 bundle config --local path vendor/bundle
-bundle install --jobs=7 --retry=3 --verbose
+bundle install --jobs=7 --retry=3
 
 echo "+++ bundle exec task"
 bundle exec rake
