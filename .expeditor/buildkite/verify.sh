ae74d37433d9d194b40d9f5161067066c42c6122
Pankaj Banshiwal <pbanshi@progress.com>
2025-06-20 19:46:40 +0530

    libgit2-dev cmake pkg-config-added
    Signed-off-by: Pankaj Banshiwal <pbanshi@progress.com>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -2,6 +2,7 @@
 export ASDF_RUBY_VERSION=$(cat .ruby-version)
 ruby --version
 bundler --version
+sudo apt install libgit2-dev cmake pkg-config
 bundle config set path 'vendor/bundle'
 bundle install
 curl https://sh.rustup.rs -sSf | sh -s -- -y
