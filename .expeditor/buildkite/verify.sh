c9e6ddda0f96077ccc28b5a6f51c7bd241775cc2
Pankaj Banshiwal <pbanshi@progress.com>
2025-06-20 20:16:58 +0530

    libgit2-dev-removed
    Signed-off-by: Pankaj Banshiwal <pbanshi@progress.com>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -2,7 +2,7 @@
 export ASDF_RUBY_VERSION=$(cat .ruby-version)
 ruby --version
 bundler --version
-sudo apt install libgit2-dev cmake pkg-config
+sudo apt install cmake pkg-config
 bundle config set path 'vendor/bundle'
 bundle install
 curl https://sh.rustup.rs -sSf | sh -s -- -y
