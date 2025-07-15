d81b50ccd6e06dbd51146003dbaad1f3962672de
Pankaj Banshiwal <pbanshi@progress.com>
2025-07-15 16:44:43 +0530

    minor changes

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -1,5 +1,7 @@
 # We currently use asdf to manage versions
 export ASDF_RUBY_VERSION=$(cat .ruby-version)
+asdf install ruby "$ASDF_RUBY_VERSION"
+asdf local ruby "$ASDF_RUBY_VERSION"
 ruby --version
 bundler --version
 bundle config set path 'vendor/bundle'
