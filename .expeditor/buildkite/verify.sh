98d4c63ec97081daa6f793896362914b9c7210e7
Pankaj Banshiwal <pbanshi@progress.com>
2025-07-15 17:34:31 +0530

    verify.sh pipeline verifications
    Signed-off-by: Pankaj Banshiwal <pbanshi@progress.com>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -1,13 +1,4 @@
 # We currently use asdf to manage versions
-# Install asdf if not present
-if ! command -v asdf >/dev/null 2>&1; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.16.1
-  . "$HOME/.asdf/asdf.sh"
-fi
-
-# Add asdf to PATH and source it
-. "$HOME/.asdf/asdf.sh"
-
 export ASDF_RUBY_VERSION=$(cat .ruby-version)
 ruby --version
 bundler --version
